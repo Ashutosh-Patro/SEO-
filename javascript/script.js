eeb70f635b315bf830b64ee42e475b9663d646c5
Ashutosh Patro <ashutosh@itobuz.com>
2023-02-20 14:21:49 +0530

    5th commit

--- javascript/script.js
+++ javascript/script.js
@@ -1,47 +1,31 @@
 let card = document.querySelectorAll(".slider-card")
 let circle = document.querySelectorAll(".circle")
 let price = document.querySelectorAll(".plan")
-let le = document.querySelectorAll('.le')
-let btn = document.querySelectorAll('.price-btn')
-
-// let i = 0;
-
-// setInterval(() => {
-//     if (i < card.length - 1) {
-//         card[i].classList.remove("current-card")
-//         circle[i].classList.remove("activated")
-//         i++
-//         card[i].classList.add("current-card")
-//         circle[i].classList.add("activated")
-//     }
-//     else {
-//         card[i].classList.remove("current-card")
-//         circle[i].classList.remove("activated")
-//         i = 0
-//         card[i].classList.add("current-card")
-//         circle[i].classList.add("activated")
-//     }
-// }, 1500)
+let learnMore = document.querySelectorAll('.choose-card-items')
 
 price.forEach((card) => {
     card.addEventListener("mouseover", () => {
         card.firstElementChild.firstElementChild.style.color = "white"
+        card.lastElementChild.style.backgroundColor = "#FAD725"
     })
 })
 price.forEach((card) => {
     card.addEventListener("mouseout", () => {
         card.firstElementChild.firstElementChild.style.color = "#6f77f1"
+        card.lastElementChild.style.backgroundColor = "white"
     })
 })
 
-price.forEach((button) => {
-    button.addEventListener("mouseover", () => {
-        button.lastElementChild.style.backgroundColor = "#FAD725"
+learnMore.forEach((learnBtn) => {
+    learnBtn.addEventListener("mouseover", () => {
+        learnBtn.lastElementChild.lastElementChild.style.backgroundColor = "#FAD725"
+        learnBtn.lastElementChild.lastElementChild.style.color = "black"
     })
 })
-price.forEach((button) => {
-    button.addEventListener("mouseout", () => {
-        button.lastElementChild.style.backgroundColor = "white"
+learnMore.forEach((learnBtn) => {
+    learnBtn.addEventListener("mouseout", () => {
+        learnBtn.lastElementChild.lastElementChild.style.backgroundColor = "white"
+        learnBtn.lastElementChild.lastElementChild.style.color = "#FAD725"
     })
 })
 
