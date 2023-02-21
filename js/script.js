b78129d300c1f747c8911eb4448a56a817ebcabc
Ashutosh Patro <ashutosh@itobuz.com>
2023-02-21 18:42:58 +0530

    21th feb 2nd

--- js/script.js
+++ js/script.js
@@ -1,33 +1,4 @@
 let card = document.querySelectorAll(".slider-card")
-let circle = document.querySelectorAll(".circle")
-let price = document.querySelectorAll(".plan")
-let learnMore = document.querySelectorAll('.choose-card-items')
-
-price.forEach((card) => {
-    card.addEventListener("mouseover", () => {
-        card.firstElementChild.firstElementChild.style.color = "white"
-        card.lastElementChild.style.backgroundColor = "#FAD725"
-    })
-})
-price.forEach((card) => {
-    card.addEventListener("mouseout", () => {
-        card.firstElementChild.firstElementChild.style.color = "#6f77f1"
-        card.lastElementChild.style.backgroundColor = "white"
-    })
-})
-
-learnMore.forEach((learnBtn) => {
-    learnBtn.addEventListener("mouseover", () => {
-        learnBtn.lastElementChild.lastElementChild.style.backgroundColor = "#FAD725"
-        learnBtn.lastElementChild.lastElementChild.style.color = "black"
-    })
-})
-learnMore.forEach((learnBtn) => {
-    learnBtn.addEventListener("mouseout", () => {
-        learnBtn.lastElementChild.lastElementChild.style.backgroundColor = "white"
-        learnBtn.lastElementChild.lastElementChild.style.color = "#FAD725"
-    })
-})
 
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 1,
